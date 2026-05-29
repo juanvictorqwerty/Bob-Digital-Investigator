ab45a13805d347cb1c65d71cac7b6f74c1504ccc
Juan <juanvictorqwerty@gmail.com>
2026-05-29 15:26:48 +0100

    refactor: consolidate authentication routes into a single include in project URLs

--- server/_Project/urls.py
+++ server/_Project/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from authentication.views import UserRegistrationView,UserLoginView
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/',UserRegistrationView.as_view(),name='signup'),
-    path('login/',UserLoginView.as_view(),name='login')
+    path('', include('authentication.urls')),
 ]
